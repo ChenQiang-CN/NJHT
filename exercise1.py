738dc902fc24c65f4e5fab63c83c7ad9eb3dcc27
stevengang <87931021+stevengang@users.noreply.github.com>
2021-07-28 10:08:26 +0800

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -7,7 +7,7 @@ os.system('pip list')
 # question2
 from osgeo import gdal
 
-dataset = gdal.Open(r' D:\tif\tif\picture_3857.tif ')
+dataset = gdal.Open(r' D:\tif\tif\picture_0000.tif ')
 proj = dataset.GetProjection()
 width = dataset.RasterXSize
 height = dataset.RasterYSize
