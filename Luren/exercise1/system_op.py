2f9333021e14d331bdce3ea1de639cf56b9b019f
Burniezz <48086010+Burniezz@users.noreply.github.com>
2021-08-08 05:47:15 +0800

    Assign 1

--- Luren/exercise1/system_op.py
+++ Luren/exercise1/system_op.py
@@ -1,15 +1,34 @@
+# -*- coding:utf-8 -*-
+# Created on 2021/7/26 by luren
 
+import numpy, pandas, netCDF4, h5py
 import os
 from osgeo import gdal
 
-# Question 1
-os.system('conda list')
 
-# Question 2
-dataset = gdal.Open('../../样例数据/true_color_3857.tif')
-print('Projection: %s' % dataset.GetProjection())
+# Question 1:
+# 在控制台中输出当前python环境下的所有第三方库及版本
+pkgs = os.popen('conda list', mode='r').readlines()
+for _ in pkgs:
+    print(_, end='')
 
-x_size = dataset.RasterXSize
-y_size = dataset.RasterYSize
-print('wide: %4d' % x_size, 'length: %4d' % y_size)
+
+# Question 2:
+# 使用GDAL读取样例栅格文件，在控制台中输出文件的投影信息、行列数
+file_path = '样例数据/true_color_3857.tif'
+dataset = gdal.Open(file_path, gdal.GA_ReadOnly)
+
+if not dataset:
+    raise Exception('Invalid file path!')
+else:
+    projection = dataset.GetProjection()
+    rows = dataset.RasterYSize
+    cols = dataset.RasterXSize
+    bands = dataset.RasterCount
+
+print(f'The size of image is:\n{rows} rows, {cols} columns and {bands} bands.')
+print(f'The projection of image is:{projection}')
+
+
+
 
